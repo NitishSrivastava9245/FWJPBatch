0592662c6062f6c14f064ecc20b9115b1776943f
NitishSrivastava9245 <150230725+NitishSrivastava9245@users.noreply.github.com>
2025-01-23 16:21:31 +0530

    Add files via upload

---
+++ day3/Q4a.java
@@ -0,0 +1,15 @@
+// Area of circle -new
+import java.util.*;
+class Q4a
+{
+public static void main(String args[])
+{
+float pi=3.14f;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the radius of circle: ");
+float r=sc.nextFloat();
+float area= pi*r*r;
+
+System.out.println("Area of Circle : "+area);
+}
+}
