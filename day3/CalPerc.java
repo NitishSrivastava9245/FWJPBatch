a186b1653014d449fe4a5422ee9e6fb2108856f5
NitishSrivastava9245 <150230725+NitishSrivastava9245@users.noreply.github.com>
2025-01-23 15:43:43 +0530

    Add files via upload

---
+++ day3/CalPerc.java
@@ -0,0 +1,13 @@
+// write a program for calculate percentage.
+
+class CalPerc
+{
+  public static void main(String args[])
+{
+  float marksObt=430;
+  float totalMarks= 500;
+
+float perc= ((marksObt / totalMarks) * 100);
+System.out.println(perc);
+}
+}
