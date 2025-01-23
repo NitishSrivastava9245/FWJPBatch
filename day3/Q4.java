b90092e71a822d1cb2764591cd910c7544869ec7
NitishSrivastava9245 <150230725+NitishSrivastava9245@users.noreply.github.com>
2025-01-23 15:43:01 +0530

    Add files via upload

---
+++ day3/Q4.java
@@ -0,0 +1,13 @@
+// Area of circle 
+
+class Q4
+{
+public static void main(String args[])
+{
+float pi=3.14f;
+float r=3.5f;
+float area= pi*r*r;
+
+System.out.println("Area of Circle : "+area);
+}
+}
