d865357e94508c9152aad032a2ea91c64bbf0cef
NitishSrivastava9245 <150230725+NitishSrivastava9245@users.noreply.github.com>
2025-01-23 15:53:29 +0530

    Add files via upload

---
+++ day1/FirstDemo.java
@@ -0,0 +1,15 @@
+class FirstDemo
+{
+public static void main(String args[])
+{
+  
+ // single line comments
+
+/*   multiline comment
+*/
+
+// to print a message on console screen 
+System.out.println("Hello World");
+}
+}
+
