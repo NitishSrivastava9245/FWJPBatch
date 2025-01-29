bb38ceaf968c9b2bbbd2df56284ef693a26baeeb
NitishSrivastava9245 <150230725+NitishSrivastava9245@users.noreply.github.com>
2025-01-29 15:29:31 +0530

    Add files via upload

---
+++ Day6/Lms assignment/Employee.java
@@ -0,0 +1,20 @@
+public class Employee {
+ protected
+  //int age;
+  //age=(int)35.5;
+   int id;
+   boolean ispermanent;
+   String name;  //
+ // int age=35.5;    // error: incompatible types: possible lossy conversion from double to int
+
+ int age=(int)35.5;
+
+  
+   
+   public static void main(String args[])
+   {
+      System.out.print("Succesfully started");
+   }
+
+    
+}
---
+++ Day6/Lms assignment/Student.java
@@ -0,0 +1,13 @@
+class Student 
+{
+  private
+    String studentName;
+    String collegeName;
+    int studentID;
+  public static void main(String []args)
+  {
+    
+     System.out.print("Succesful");
+
+  }
+}
